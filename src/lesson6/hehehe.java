d7200e3348b2b17d868a6c05f2d54b1374d1c378
Administrator <kimmothy@naver.com>
2017-11-09 16:01:33 +0900

    sdf

--- src/lesson6/hehehe.java
+++ src/lesson6/hehehe.java
@@ -3,6 +3,7 @@ package lesson6;
 public class hehehe {
     public static void main(String[] arg){
         System.out.println("hello world");
+        System.out.println("hello world");
 
     }
 }
